416e08b016c666d5e72eeef7827fff272ecf10d0
Студент <eliza2017.zimina@mail.ru>
2023-07-21 13:55:04 +0300

    static function for reading

--- configReader/configReader.h
+++ configReader/configReader.h
@@ -2,7 +2,53 @@
 #define TCPUDP_CONFIGREADER_H
 
 
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QFile>
+#include <QDebug>
+
 class configReader {
+
+public :
+    static void print(const QString& filePath){
+
+        QFile file(filePath);
+        if (!file.open(QIODevice::ReadOnly)) {
+            qWarning() << "Failed to open the JSON file.";
+            return;
+        }
+
+        // Чтение содержимого файла и его парсинг как JSON
+        QByteArray jsonData = file.readAll();
+        QJsonDocument document = QJsonDocument::fromJson(jsonData);
+
+        if (document.isNull()) {
+            qWarning() << "Failed to parse the JSON file.";
+            return;
+        }
+
+        // Теперь вы можете работать с данными из файла JSON
+        QJsonObject jsonObject = document.object();
+
+        // Пример доступа к значениям по ключам:
+        if (jsonObject.contains("udp")) {
+            QJsonObject udpObject = jsonObject["udp"].toObject();
+            QString host = udpObject["host"].toString();
+            int port= udpObject["port"].toInt();
+
+            qDebug() << "UDP Host: " << host;
+            qDebug() << "UDP Port: " << port;
+        }
+
+        if (jsonObject.contains("tcp")) {
+            QJsonObject udpObject = jsonObject["tcp"].toObject();
+            QString host = udpObject["host"].toString();
+            int port = udpObject["port"].toInt();
+
+            qDebug() << "UDP Host: " << host;
+            qDebug() << "UDP Port: " << port;
+        }
+    }
 
 };
 
--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include <QCoreApplication>
 #include <QDebug>
 #include <QTime>
-#include <QJsonObject>
-#include <QJsonDocument>
-#include <QFile>
 #include <iostream>
 #include "tcp/tcpclient.h"
 #include "tcp/tcpserver.h"
 #include "udp/udpreceiver.h"
+#include "configReader/configReader.h"
 
 const extern quint16 PORT = 5000;
 
@@ -18,75 +16,39 @@ void delay()
         QCoreApplication::processEvents(QEventLoop::AllEvents, 100);
 }
 
-
-
-void readJsonFile(const QString& filePath) {
-    QFile file(filePath);
-    if (!file.open(QIODevice::ReadOnly)) {
-        qWarning() << "Failed to open the JSON file.";
-        return;
-    }
-
-    // Чтение содержимого файла и его парсинг как JSON
-    QByteArray jsonData = file.readAll();
-    QJsonDocument document = QJsonDocument::fromJson(jsonData);
-
-    if (document.isNull()) {
-        qWarning() << "Failed to parse the JSON file.";
-        return;
-    }
-
-    // Теперь вы можете работать с данными из файла JSON
-    QJsonObject jsonObject = document.object();
-
-    // Пример доступа к значениям по ключам:
-    if (jsonObject.contains("udp")) {
-        QJsonObject udpObject = jsonObject["udp"].toObject();
-        QString host = udpObject["host"].toString();
-        int port= udpObject["port"].toInt();
-
-        qDebug() << "UDP Host: " << host;
-        qDebug() << "UDP Port: " << port;
-    }
-}
-
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
 
-    readJsonFile("C:/Users/Студент/CLionProjects/tcpudp/config.json");
+    configReader::print("C:/Users/Студент/CLionProjects/tcpudp/config.json");
 
-    ///TCP ///
-    std::cout << "--- TCP Client ---" << std::endl;
-    TcpClient tc;
-
-    std::cout << "--- TCP Server ---" << std::endl;
-    TcpServer ts;
-
-    delay();
+//    ///TCP ///
+//    std::cout << "--- TCP Client ---" << std::endl;
+//    TcpClient tc;
+//
+//    std::cout << "--- TCP Server ---" << std::endl;
+//    TcpServer ts;
+//
+//    delay();
 
     ///UDP///
 
-
-
-
-    UdpReceiver ur;
-
-    QUdpSocket qus;
-
-    qus.bind(QHostAddress("127.0.0.1"), PORT+1);
-
-    QByteArray msg = "Hello world!";
-
-    std::cout << "--- Sender ---" << std::endl;
-
-    for(int i = 0; i < 10; ++i)
-        qus.writeDatagram(msg, QHostAddress("127.0.0.1"), PORT);
-
-
-    std::cout << "--- Recevier ---" << std::endl;
-
-
-
+//    UdpReceiver ur;
+//
+//    QUdpSocket qus;
+//
+//    qus.bind(QHostAddress("127.0.0.1"), PORT+1);
+//
+//    QByteArray msg = "Hello world!";
+//
+//    std::cout << "--- Sender ---" << std::endl;
+//
+//    for(int i = 0; i < 10; ++i)
+//        qus.writeDatagram(msg, QHostAddress("127.0.0.1"), PORT);
+//
+//
+//    std::cout << "--- Recevier ---" << std::endl;
 
     return QCoreApplication::exec();
 }
+
+#include "main.moc"
