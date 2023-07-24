aa694f8c6a97337fe9f85c90f9dce5e30e288ca9
Студент <eliza2017.zimina@mail.ru>
2023-07-24 12:19:49 +0300

    reading udp/tcp data from json file

--- configReader/configReader.h
+++ configReader/configReader.h
@@ -14,36 +14,65 @@ private :
 
     static void readJson(QJsonObject jsonObject) {
 
+    //count of sokets
+        int count = 0;
+
+        /*     Reading CONFIG INFO    */
 
         if (jsonObject.contains("config")) {
+
             QJsonObject confObject = jsonObject["config"].toObject();
-            int count = confObject["count"].toInt();
-            std::cout << count;
+
+            if(confObject.contains("count")){
+
+                count = confObject["count"].toInt();
+
+            }
+            else{
+
+                std::cout << "wrong config information\n";
+                return;
+
+            }
+
         }
         else{
             std::cout << "wrong config information\n";
             return;
         }
 
+        /*     reading json data   */
 
-//
-//        if (jsonObject.contains("udp")) {
-//            QJsonObject udpObject = jsonObject["udp"].toObject();
-//            QString host = udpObject["host"].toString();
-//            int port= udpObject["port"].toInt();
-//
-//            qDebug() << "UDP Host: " << host;
-//            qDebug() << "UDP Port: " << port;
-//        }
-//
-//        if (jsonObject.contains("tcp")) {
-//            QJsonObject udpObject = jsonObject["tcp"].toObject();
-//            QString host = udpObject["host"].toString();
-//            int port = udpObject["port"].toInt();
-//
-//            qDebug() << "UDP Host: " << host;
-//            qDebug() << "UDP Port: " << port;
-//        }
+        for(int i = 0; i < count ; ++i){
+
+            QJsonObject newObject = jsonObject[QString::number(i)].toObject();
+
+
+            /*    parse UDP  */
+
+            if (newObject.contains("udp")) {
+
+                QJsonObject udpObject = newObject["udp"].toObject();
+                QString host = udpObject["host"].toString();
+                int port = udpObject["port"].toInt();
+
+                qDebug() << "UDP Host: " << host;
+                qDebug() << "UDP Port: " << port;
+            }
+
+            /*    parse TCP    */
+
+            else if (newObject.contains("tcp")) {
+
+                QJsonObject tcpObject = newObject["tcp"].toObject();
+                QString host = tcpObject["host"].toString();
+                int port = tcpObject["port"].toInt();
+
+                qDebug() << "TCP Host: " << host;
+                qDebug() << "TCP Port: " << port;
+            }
+
+        }
 
     }
 
@@ -52,6 +81,8 @@ public:
     static void print(const QString& filePath){
 
         QFile file(filePath);
+
+        //Fail parsing
         if (!file.open(QIODevice::ReadOnly)) {
             qWarning() << "Failed to open the JSON file.";
             return;
